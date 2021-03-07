49d7adc9fee7ac25967101043a8438a3d9dca0f7
majasol <solheimmaja@gmail.com>
2021-03-07 22:01:33 +0100

    Update app.py

--- Flask_avsluttende/app.py
+++ Flask_avsluttende/app.py
@@ -42,8 +42,10 @@ def liste(sorter):
     return render_template('liste.html', biler=biler)
 
 
-@app.route('/liste/enkel')
-def enkel():
+@app.route('/liste/enkel/<id>')
+def enkel(id):
+    biler = db.engine.execute(
+        f'SELECT * FROM biler WHERE id={id}')
     return render_template('enkel.html', biler=biler)
 
 
